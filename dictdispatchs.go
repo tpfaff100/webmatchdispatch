45433f9eef3d778ae26762d5ff251d1c77c9be38
T C P <tpfaff100@gmail.com>
2025-01-03 13:04:05 -0800

    Update dictdispatchs.go
    Added documentation in golang file describing how to build and run

--- dictdispatchs.go
+++ dictdispatchs.go
@@ -1,3 +1,26 @@
+/*
+
+Usage:
+
+------------------
+Using OPENSSL, generate unsafe https key so we can run an https server easily:
+openssl genrsa 2048 > server.key
+chmod 400 server.key
+openssl req -new -x509 -nodes -sha256 -days 365 -key server.key -out server.cert
+-------------------
+go build *.go
+./dictdispatchs  
+
+Then load the web browser up and try two links:
+
+https://localhost:9876/login
+https://localhost:9876
+
+This example shows how to setup function dispatch tables in golang
+Not hard.  I just haven't seen it done anywhere yet.
+
+*/
+
 package main
 
 import (
